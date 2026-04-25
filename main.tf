69a36c92427dd586079643b0dfe5e9d51d29decd
Nurgali <nurgali.anarbek04@mail.ru>
2026-04-25 12:27:55 +0500

    теперь terraform fmt      → проверяет форматирование кода terraform validate → проверяет синтаксис кода terraform apply    → применяет изменения

--- main.tf
+++ main.tf
@@ -1,34 +1,34 @@
 terraform {
   required_providers {
     local = {
-        source = "hashicorp/local"
-        version = "~> 2.0"
+      source  = "hashicorp/local"
+      version = "~> 2.0"
     }
   }
 }
 
 module "backend" {
-  source = "./modules/file_creator"
-  app_name = "backend"
+  source      = "./modules/file_creator"
+  app_name    = "backend"
   environment = "production"
 }
 module "frontend" {
-  source = "./modules/file_creator"
-  app_name = "frontend"
+  source      = "./modules/file_creator"
+  app_name    = "frontend"
   environment = "production"
 }
 module "database" {
-  source   = "./modules/file_creator"
-  app_name = "database"
+  source      = "./modules/file_creator"
+  app_name    = "database"
   environment = "staging"
 }
 resource "local_file" "server" {
-  count = 3
+  count    = 3
   filename = "server-${count.index}.txt"
-  content = "server number: ${count.index}" 
+  content  = "server number: ${count.index}"
 }
 resource "local_file" "Gorod" {
-  for_each = toset(["almaty","shymkent","astana"])
+  for_each = toset(["almaty", "shymkent", "astana"])
   filename = "${each.key}.txt"
-  content = "City: ${each.key}"
+  content  = "City: ${each.key}"
 }
